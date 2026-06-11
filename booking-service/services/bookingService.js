b65c36ca555b04d0cb0acc7633bec28cb4ecd47d
MohsinKhan <khanmdmohsin1802@gmail.com>
2026-06-11 15:10:24 +0530

    add: service-layer for booking service

---
+++ booking-service/services/bookingService.js
@@ -0,0 +1,35 @@
+import bookingRepository from "../repositories/bookingRepository.js";
+import AppError from "../errors/AppError.js";
+
+class BookingService {
+  async createBooking(data) {
+    // we will call the Catalog Service here to check if the event exists
+    // and if there are enough availableSeats.
+
+    const price = 100;
+    const totalPrice = data.ticketCount * price;
+
+    const bookingData = {
+      eventId: data.eventId,
+      userId: data.userId,
+      ticketCount: data.ticketCount,
+      totalPrice: totalPrice,
+      status: "CONFIRMED",
+    };
+
+    const newBooking = await bookingRepository.createBooking(bookingData);
+
+    return newBooking;
+  }
+
+  async getBookingById(id) {
+    const booking = await bookingRepository.getBookingById(id);
+
+    if (!booking) {
+      throw new AppError("Booking not found", 404);
+    }
+    return booking;
+  }
+}
+
+export default BookingService;
